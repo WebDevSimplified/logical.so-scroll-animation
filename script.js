e9d47308e3923807b194f033d5f7c6158cd03fe3
Web Dev Simplified <kyle@webdevsimplified.com>
2023-06-16 14:55:01 -0500

    Finish Video

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+
+function setScrollVar() {
+  const htmlElement = document.documentElement
+  const percentOfScreenHeightScrolled =
+    htmlElement.scrollTop / htmlElement.clientHeight
+  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
+  htmlElement.style.setProperty(
+    "--scroll",
+    Math.min(percentOfScreenHeightScrolled * 100, 100)
+  )
+}
+
+setScrollVar()
+
+const observer = new IntersectionObserver(entries => {
+  for (let i = entries.length - 1; i >= 0; i--) {
+    const entry = entries[i]
+    if (entry.isIntersecting) {
+      document.querySelectorAll("[data-img]").forEach(img => {
+        img.classList.remove("show")
+      })
+      const img = document.querySelector(entry.target.dataset.imgToShow)
+      img?.classList.add("show")
+      break
+    }
+  }
+})
+
+document.querySelectorAll("[data-img-to-show]").forEach(section => {
+  observer.observe(section)
+})
